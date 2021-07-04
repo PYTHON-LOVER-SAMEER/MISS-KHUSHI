f406b88f909b9573eafc8af44dd813b895fb357f
Abdullah AL-Noman <83629146+AL-Noman21@users.noreply.github.com>
2021-07-04 13:39:44 +0600

    Add files via upload

--- SaitamaRobot/modules/telegraph.py
+++ SaitamaRobot/modules/telegraph.py
@@ -1,6 +1,6 @@
 # COPYRIGHT (C) BY LEGENDX22 AND PROBOYX
-from LEGEND.events import register
-from LEGEND import telethn as tbot
+from SaitamaRobot.events import register
+from SaitamaRobot import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "./"
 from telethon import events
 import os
